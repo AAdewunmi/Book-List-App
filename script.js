581cf946502c158d7ce1b55ab48ef345fe3efee0
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-03-29 19:06:30 +0000

    Eighteen Commit: Implement showAlerts() function

--- script.js
+++ script.js
@@ -31,4 +31,20 @@ class UI {
       el.parentElement.parentElement.remove();
     }
   }
+
+  static showAlerts(message, className) {
+    const div = document.createElement("div");
+
+    div.className = `alert alert-${className}`;
+
+    div.appendChild(document.createTextNode(message));
+
+    const container = document.querySelector(".container");
+    const form = document.querySelector("#book-form");
+
+    container.insertBefore(div, form);
+
+    //Vanish in 3 seconds
+    setTimeout(() => document.querySelector(".alert").remove(), 3000);
+  }
 }
